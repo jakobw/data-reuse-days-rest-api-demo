a3c556c4bde90641c2456439f26ac193c16fde36
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2025-01-29 19:07:29 +0100

    Enable editing types

--- app.js
+++ app.js
@@ -15,14 +15,17 @@ app.get( '/', ( req, res ) => {
 	res.render( 'index', { user: req.session.user } );
 } );
 
-async function makeApiRequest( path ) {
+async function makeApiRequest( path, method = 'GET', jsonBody ) {
 	const restApiBaseUrl = 'http://default.mediawiki.mwdd.localhost:8080/w/rest.php/wikibase/v1';
 	return ( await fetch(
 		restApiBaseUrl + path,
 		{
+			method,
 			headers: {
-				'User-Agent': `DataReuseDaysDemo/0.0 (${ process.env.EMAIL })`
-			}
+				'User-Agent': `DataReuseDaysDemo/0.0 (${ process.env.EMAIL })`,
+				'Content-Type': 'application/json'
+			},
+			body: jsonBody && JSON.stringify( jsonBody )
 		}
 	) ).json();
 }
@@ -57,6 +60,68 @@ app.get( '/pokemon', async ( req, res ) => {
 		secondaryTypeStatement,
 		allTypes: ids.allTypes
 	} );
+} );
+
+async function addTypeStatement( itemId, value, typeQualifierValue ) {
+	return makeApiRequest(
+		`/entities/items/${ itemId }/statements`,
+		'POST',
+		{
+			statement: {
+				property: { id: ids.instanceOf },
+				value: { type: 'value', content: value },
+				qualifiers: [ {
+					property: { id: ids.appliesToPart },
+					value: { type: 'value', content: typeQualifierValue }
+				} ]
+			}
+		}
+	);
+}
+
+async function deleteTypeStatement( statementId ) {
+	return makeApiRequest( `/statements/${ statementId }`, 'DELETE' );
+}
+
+async function changeTypeStatementValue( statementId, value ) {
+	return makeApiRequest(
+		`/statements/${ statementId }`,
+		'PATCH',
+		{
+			patch: [
+				{ op: 'replace', path: '/value/content', value: value }
+			]
+		}
+	);
+}
+
+app.post( '/update-types', async ( req, res ) => {
+	const updates = [
+		{
+			statementId: req.body.primaryStatementId,
+			newType: req.body.primaryType,
+			originalType: req.body.originalPrimaryType,
+			typeQualifierValue: ids.primaryType
+		},
+		{
+			statementId: req.body.secondaryStatementId,
+			newType: req.body.secondaryType,
+			originalType: req.body.originalSecondaryType,
+			typeQualifierValue: ids.secondaryType
+		},
+	];
+
+	for ( const { statementId, newType, originalType, typeQualifierValue } of updates ) {
+		if( !statementId && newType !== 'none' ) {
+			await addTypeStatement( req.body.id, newType, typeQualifierValue );
+		} else if( statementId && newType === 'none' ) {
+			await deleteTypeStatement( statementId );
+		} else if( statementId && newType !== originalType ) {
+			await changeTypeStatementValue( statementId, newType );
+		}
+	}
+
+	res.redirect( `/pokemon?id=${req.body.id}` );
 } );
 
 app.listen( port, () => {
